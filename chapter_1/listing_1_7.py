37e0b2a9c2028d3506b6cfd29acf8e1a67da8989
Artem <artem-sherbakov-1998@mail.ru>
2022-11-17 23:47:49 +0300

    Синхронное чтение кода состояния

---
+++ chapter_1/listing_1_7.py
@@ -0,0 +1,21 @@
+"""
+Синхронное чтение кода состояния
+"""
+
+import time
+import requests
+
+
+def read_example() -> None:
+    response = requests.get('https://ya.ru/')
+    print(response.status_code)
+
+
+sync_start = time.time()
+
+read_example()
+read_example()
+
+sync_end = time.time()
+
+print(f'Синхронное выполнение заняло {sync_end - sync_start:.4f} с.')
