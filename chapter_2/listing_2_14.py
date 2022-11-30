911c7a98a5d74279c6049ab9836dc2de45f501a4
Artem <artem-sherbakov-1998@mail.ru>
2022-11-30 20:56:51 +0300

    Основы будущих объектов

---
+++ chapter_2/listing_2_14.py
@@ -0,0 +1,12 @@
+from asyncio import Future
+
+
+my_future = Future()
+
+print(f'my_future готов? {my_future.done()}')
+
+my_future.set_result(42)
+
+print(f'my_future готов? {my_future.done()}')
+
+print(f'Какой результат хранится в my_future? {my_future.result()}')
