be0ef69b7d988982eeaa1af748deb37413bbb0a4
Artem <artem-sherbakov-1998@mail.ru>
2023-01-16 23:11:57 +0300

    Добавление обработчика сигнала, снимающего все задачи

---
+++ chapter_3/listing_3_9.py
@@ -0,0 +1,21 @@
+import asyncio, signal
+from asyncio import AbstractEventLoop
+from typing import Set
+
+from util import delay
+
+def cancel_tasks():
+    print('Получен сигнал SIGINT!')
+    tasks: Set[asyncio.Task] = asyncio.all_tasks()
+    print(f'Снимается {len(tasks)} задач.')
+    [task.cancel() for task in tasks]
+
+async def main():
+    loop: AbstractEventLoop = asyncio.get_event_loop()
+
+    loop.add_signal_handler(signal.SIGINT, cancel_tasks)
+
+    await delay(10)
+
+
+asyncio.run(main())
