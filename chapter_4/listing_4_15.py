c3804b82132be9fa6063f9d0790a913090a15a86
Artem <artem-sherbakov-1998@mail.ru>
2023-02-02 23:14:16 +0300

    Использование таймаутов в wait

---
+++ chapter_4/listing_4_15.py
@@ -0,0 +1,27 @@
+"""Использование таймаутов в wait"""
+import asyncio
+import aiohttp
+from util import async_timed
+from chapter_4 import fetch_status
+
+
+@async_timed()
+async def main():
+    async with aiohttp.ClientSession() as session:
+
+        url = 'https://ya.ru/'
+        fetchers = [asyncio.create_task(fetch_status(session, url)),
+                    asyncio.create_task(fetch_status(session, url)),
+                    asyncio.create_task(fetch_status(session, url, delay=3))]
+
+        # done - содержит завершившиеся задачи или в результате исключения pending - ожидающие задачи
+        done, pending = await asyncio.wait(fetchers, timeout=1)
+
+        print(f'Число завершившихся задач: {len(done)}')
+        print(f'Число ожидающих задач: {len(pending)}')
+
+        for done_task in done:
+            res = await done_task
+            print(res)
+
+asyncio.run(main())
