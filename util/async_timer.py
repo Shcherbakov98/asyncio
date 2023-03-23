3229df8ce18711b35cab70874d85346a71103db1
Artem <artem-sherbakov-1998@mail.ru>
2023-03-24 01:59:10 +0300

    Цикл событий в каждом процессе

--- util/async_timer.py
+++ util/async_timer.py
@@ -17,3 +17,16 @@ def async_timed():
                 print(f'{func} завершилась за {total:.4f} с')
         return wrapped
     return wrapper
+
+# def async_timed(func: Callable):
+#     @functools.wraps(func)
+#     def wrapper(*args, **kwargs) -> Callable:
+#         print(f'выполняется {func}, с аргументами {args} {kwargs}')
+#         start = time.time()
+#         try:
+#             return await func(*args, **kwargs)
+#         finally:
+#             end = time.time()
+#             total = end - start
+#             print(f'{func} завершилась за {total:.4f} с')
+#     return wrapper
