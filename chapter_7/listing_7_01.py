c34310656f5bea0d636b13adcaf647a56d5c937e
Artem <artem-sherbakov-1998@mail.ru>
2023-04-14 00:00:53 +0300

    Многопоточный эхо-сервер

---
+++ chapter_7/listing_7_01.py
@@ -0,0 +1,19 @@
+"""Многопоточный эхо-сервер"""
+from threading import Thread
+import socket
+
+
+def echo(client: socket):
+    while True:
+        data = client.recv(2048)
+        print(f'Получено {data}, отправляю!')
+        client.sendall(data)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(('127.0.0.1', 8000))
+    server.listen()
+    while True:
+        conn, _ = server.accept() # блокируется в ожидании клиентов
+        thread = Thread(target=echo, args=(conn, )) # как только клиент подключился, создать поток для выполнения
+        thread.start() # начать выполнение потока
