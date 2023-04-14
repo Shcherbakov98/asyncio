259fba2d71cf023bcecc2390098b1bdbed9b4372
Artem <artem-sherbakov-1998@mail.ru>
2023-04-15 00:31:51 +0300

    Базовое использование request

---
+++ chapter_7/listing_7_03.py
@@ -0,0 +1,12 @@
+"""Базовое использование request"""
+import requests
+
+
+def get_status_code(url: str) -> int:
+    response = requests.get(url)
+    return response.status_code
+
+
+url = 'https://www.example.com'
+print(get_status_code(url))
+print(get_status_code(url))
