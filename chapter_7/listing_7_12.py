bc7b4a5d7f76d156956e58498d6c9e986edafdff
Artem <artem-sherbakov-1998@mail.ru>
2023-05-13 17:00:25 +0300

    Приложение 'hello world' на TKinter

---
+++ chapter_7/listing_7_12.py
@@ -0,0 +1,19 @@
+"""Приложение 'hello world' на TKinter"""
+import tkinter
+from tkinter import ttk
+
+
+window = tkinter.Tk()
+window.title('Hello world app')
+window.geometry('200x100')
+
+
+def say_hello():
+    print('Привет!')
+
+
+hello_button = ttk.Button(window, text='Say hello', command=say_hello)
+hello_button.pack()
+
+
+window.mainloop()
