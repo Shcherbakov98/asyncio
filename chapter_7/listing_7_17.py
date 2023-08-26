7a6178d2ffa314414debcc653173b90a9ea2f826
Artem <artem-sherbakov-1998@mail.ru>
2023-08-26 17:38:30 +0300

    Хеширование с применением многопоточности и asyncio

---
+++ chapter_7/listing_7_17.py
@@ -0,0 +1,36 @@
+"""Хеширование с применением многопоточности и asyncio"""
+import asyncio
+import functools
+import hashlib
+import os
+from concurrent.futures.thread import ThreadPoolExecutor
+import random
+import string
+from util import async_timed
+
+
+def random_password(length: int) -> bytes:
+    ascii_lowercase = string.ascii_lowercase.encode()  # b'abcdefghijklmnopqrstuvwxyz'
+    return b''.join(bytes(random.choice(ascii_lowercase)) for _ in range(length))
+
+
+passwords = [random_password(10) for _ in range(10000)]
+
+
+def hash_(password: bytes) -> str:
+    salt = os.urandom(16)
+    return str(hashlib.scrypt(password, salt=salt, n=2048, p=1, r=8))
+
+
+@async_timed()
+async def main():
+    loop = asyncio.get_running_loop()
+    tasks = []
+
+    with ThreadPoolExecutor() as pool:
+        for password in passwords:
+            tasks.append(loop.run_in_executor(pool, functools.partial(hash_, password)))
+
+    await asyncio.gather(*tasks)
+
+asyncio.run(main())
