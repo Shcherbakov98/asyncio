14983307cfa29daf7fb11e91f5b9c812fee9cfa0
Artem <artem-sherbakov-1998@mail.ru>
2023-08-26 18:25:35 +0300

    Многопоточность с Numpy

--- chapter_7/listing_7_18.py
+++ chapter_7/listing_7_18.py
@@ -3,7 +3,7 @@ import numpy as np
 import time
 
 
-data_points = 4000000000
+data_points = 1000000000
 rows = 50
 columns = int(data_points / rows)
 
@@ -16,4 +16,4 @@ res = np.mean(matrix, axis=1)
 end = time.time()
 
 print('total_time = ', end - start)
-# total_time =
+# total_time = 0.58
