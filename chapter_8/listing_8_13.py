dabc7904bf680416e7b1399d2737fca50ced3abc
Artem <artem-sherbakov-1998@mail.ru>
2023-10-24 23:30:09 +0300

    Чат сервер

---
+++ chapter_8/listing_8_13.py
@@ -0,0 +1,79 @@
+"""Чат сервер"""
+import asyncio
+import logging
+from asyncio import StreamReader, StreamWriter
+
+
+class ChatServer(object):
+    def __init__(self):
+        self._username_to_writer = {}
+
+    async def start_chat_server(self, host: str, port: int):
+        server = await asyncio.start_server(self.client_connected, host, port)
+
+        async with server:
+            await server.serve_forever()
+
+    # ждать, пока клиент отправит допустимое имя пользователя;
+    # в противном случае, отключить его
+    async def client_connected(self, reader: StreamReader, writer: StreamWriter):
+        command = await reader.readline()
+        print(f'CONNECTED {reader} {writer}')
+        command, args = command.split(b' ')
+        if command == b'CONNECTED':
+            username = args.replace(b'\n', b'').decode()
+            self._add_user(username, reader, writer)
+            await self._on_connect(username, writer)
+        else:
+            logging.error('Получена недопустимая команда от клиента, отключается')
+            writer.close()
+            await writer.wait_closed()
+
+    def _add_user(self, username: str, reader: StreamReader, writer: StreamWriter):
+        self._username_to_writer[username] = writer
+        asyncio.create_task(self._listen_for_messages(username, reader))
+
+    # после подключения пользователя, уведомить всех об этом
+    async def _on_connect(self, username: str, writer: StreamWriter):
+        writer.write(f'Добро пожаловать! Число подключенных пользователей {len(self._username_to_writer)}!\n'.encode())
+        await writer.drain()
+        await self._notify_all(f'Подключился {username}!\n')
+
+    async def _remove_user(self, username: str):
+        writer = self._username_to_writer[username]
+        del self._username_to_writer[username]
+        try:
+            writer.close()
+            await writer.wait_closed()
+        except Exception as e:
+            logging.exception('Ошибка при закрытии клиентского писателя, игнорируется.', exc_info=e)
+
+    # прослушивать сообщения от клиента, рассылать их всем остальным клиентам
+    # ждать сообщения не более минуты
+    async def _listen_for_messages(self, username: str, reader: StreamReader):
+        try:
+            while (data := await asyncio.wait_for(reader.readline(), 60)) != b'':
+                await self._notify_all(f'{username}: {data.decode()}')
+            await self._notify_all(f'{username} has left the chat\n')
+        except Exception as e:
+            logging.exception('Ошибка чтения данных от клиента.', exc_info=e)
+            await self._remove_user(username)
+
+    # отправить сообщения всем подключенным клиентам и удалить отключившихся клиентов
+    async def _notify_all(self, message: str):
+        inactive_users = []
+        for username, writer in self._username_to_writer.items():
+            try:
+                writer.write(message.encode())
+                await writer.drain()
+            except ConnectionError as e:
+                logging.exception('Ошибка при записи данных клиенту', exc_info=e)
+                inactive_users.append(username)
+        [await self._remove_user(username) for username in inactive_users]
+
+
+async def main():
+    chat_server = ChatServer()
+    await chat_server.start_chat_server('127.0.0.1', 8000)
+
+asyncio.run(main())
