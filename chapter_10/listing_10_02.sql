2b1426ef5d12f0873377c0a4bd3079510fd5ceb2
Artem <artem-sherbakov-1998@mail.ru>
2023-11-17 21:54:54 +0300

    edit tables

--- chapter_10/listing_10_02.sql
+++ chapter_10/listing_10_02.sql
@@ -1,7 +1,7 @@
 CREATE TABLE user_cart(
     user_id int not null,
     product_id int not null
-)
+);
 INSERT INTO user_cart (user_id, product_id) VALUES (1, 1);
 INSERT INTO user_cart (user_id, product_id) VALUES (1, 2);
 INSERT INTO user_cart (user_id, product_id) VALUES (1, 3);
