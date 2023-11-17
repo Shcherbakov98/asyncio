69ac6909ac8493daf8e38950acd1e0d0049de95e
Artem <artem-sherbakov-1998@mail.ru>
2023-11-17 21:48:50 +0300

    Таблица избранных товаров пользователя

---
+++ chapter_10/listing_10_03.sql
@@ -0,0 +1,10 @@
+CREATE TABLE user_favorite(
+    user_id int not null,
+    product_id int not null
+);
+INSERT INTO user_favorite (user_id, product_id) VALUES (1, 1)
+INSERT INTO user_favorite (user_id, product_id) VALUES (1, 2)
+INSERT INTO user_favorite (user_id, product_id) VALUES (1, 3)
+INSERT INTO user_favorite (user_id, product_id) VALUES (3, 1)
+INSERT INTO user_favorite (user_id, product_id) VALUES (3, 2)
+INSERT INTO user_favorite (user_id, product_id) VALUES (3, 3)
