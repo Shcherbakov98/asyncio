37846a6f34d504d37871f3767ba3010be747183c
Артем <artem-sherbakov-1998@yandex.ru>
2024-11-25 22:30:00 +0300

    Очередь с приоритетами, содержащая кортежи

---
+++ chapter_12/listing_12_05.py
@@ -0,0 +1,31 @@
+"""Очередь с приоритетами, содержащая кортежи"""
+import asyncio
+from asyncio import Queue, PriorityQueue
+from typing import Tuple
+
+
+async def worker(queue: Queue):
+    while not queue.empty():
+        work_item: Tuple[int, str] = await queue.get()
+        print(f"Обрабатывается элемент {work_item}")
+        queue.task_done()
+
+
+async def main():
+    priority_queue = PriorityQueue()
+
+    work_items = [
+        (3, 'Lowest priority'),
+        (2, 'Medium priority'),
+        (1, 'High priority'),
+    ]
+
+    worker_task = asyncio.create_task(worker(priority_queue))
+
+    for work in work_items:
+        priority_queue.put_nowait(work)
+
+    await asyncio.gather(priority_queue.join(), worker_task)
+
+
+asyncio.run(main())
