f0265261d9dc1b3f5bf5a0f1f0868c122abadf04
Артем <artem-sherbakov-1998@mail.ru>
2024-12-06 22:08:51 +0300

    Использование communicate

---
+++ chapter_13/listing_13_06.py
@@ -0,0 +1,17 @@
+"""Использование communicate"""
+import asyncio
+from asyncio.subprocess import Process
+
+
+async def main():
+    program = ['python3', 'listing_13_04.py']
+
+    process: Process = await asyncio.create_subprocess_exec(*program, stdout=asyncio.subprocess.PIPE)
+    print(f"pid процесса: {process.pid}")
+
+    stdout, stderr = await process.communicate()
+    print(stdout)
+    print(stderr)
+    print(f"Процесс вернул: {process.returncode}")
+
+asyncio.run(main())
